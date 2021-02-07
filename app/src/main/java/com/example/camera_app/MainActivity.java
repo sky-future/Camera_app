7d2d2884ab606ebd5426b6da7dff53c0120b6974
Falcon <mistert9412@gmail.com>
2021-02-07 23:05:38 +0100

    Register Window and Login Window xml

--- app/src/main/java/com/example/camera_app/MainActivity.java
+++ app/src/main/java/com/example/camera_app/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.camera_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Déclarer le bouton register et lier à la variable register pour y ajouter un clickListener
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Récupère l'ID d'un des boutons sélectionné sur l'interface de login.
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+
+        }
     }
 }
