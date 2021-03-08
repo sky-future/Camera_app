9b22c5949011f1165661cf4e58cf1eacbc10b63b
Falcon <mistert9412@gmail.com>
2021-03-08 16:38:57 +0100

    Stream camera ok

--- app/src/main/java/com/example/camera_app/CameraGridActivity.java
+++ app/src/main/java/com/example/camera_app/CameraGridActivity.java
@@ -8,22 +8,37 @@ import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.longdo.mjpegviewer.MjpegView;
 
 public class CameraGridActivity extends AppCompatActivity {
 
     private Button logout;
-    private WebView testCamera;
+
+    private ImageView testCamera;
+
+    private MjpegView mjpegView1;
+
+    private String url = "http://192.168.1.47/stream";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_grid);
 
-        testCamera = (WebView) findViewById(R.id.testCamera);
-        testCamera.setWebViewClient(new WebViewClient());
-        testCamera.loadUrl("http://192.168.1.71:81/stream");
+        //testCamera = (ImageView) findViewById(R.id.testCamera);
+        mjpegView1 = (MjpegView) findViewById(R.id.mjpegView1);
+
+       // testCamera.setVisibility(View.GONE);
+        mjpegView1.setVisibility(View.VISIBLE);
+        mjpegView1.setMode(mjpegView1.MODE_FIT_WIDTH);
+        mjpegView1.setAdjustHeight(true);
+        mjpegView1.setUrl(url);
+        mjpegView1.startStream();
+
+
 
 
         logout = (Button)findViewById(R.id.loggoutButton);
@@ -32,6 +47,7 @@ public class CameraGridActivity extends AppCompatActivity {
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(CameraGridActivity.this, MainActivity.class));
+                mjpegView1.stopStream();
             }
         });
     }
