42365023ef9bb50e8623cd63f5bd7a5cfda6d0bc
Jordan <mistert9412@gmail.com>
2021-03-09 17:38:09 +0100

    Fonctionnel

--- app/src/main/java/com/example/camera_app/CameraGridActivity.java
+++ app/src/main/java/com/example/camera_app/CameraGridActivity.java
@@ -9,33 +9,86 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.longdo.mjpegviewer.MjpegView;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import cz.msebera.android.httpclient.Header;
 
 public class CameraGridActivity extends AppCompatActivity {
 
     private Button logout;
-
-    private ImageView testCamera;
+    private Button onButton;
+    private Button offButton;
 
     private MjpegView mjpegView1;
 
-    private String url = "http://192.168.1.47/stream";
+    private String location = "http://10.104.210.29";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_grid);
 
-        //testCamera = (ImageView) findViewById(R.id.testCamera);
+        onButton = (Button)findViewById(R.id.onButton);
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Utilise une requete asynchrone afin de ne pas faire la requete dans le mainthread.
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get(location+"/on", new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        Toast.makeText(CameraGridActivity.this, "On / 200 ok", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        Toast.makeText(CameraGridActivity.this, "Erreur 404", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
+        offButton = (Button)findViewById(R.id.offButton);
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get(location+"/off", new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        Toast.makeText(CameraGridActivity.this, "Off / 200 ok", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                        Toast.makeText(CameraGridActivity.this, "Erreur 404", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+
         mjpegView1 = (MjpegView) findViewById(R.id.mjpegView1);
 
-       // testCamera.setVisibility(View.GONE);
         mjpegView1.setVisibility(View.VISIBLE);
         mjpegView1.setMode(mjpegView1.MODE_FIT_WIDTH);
         mjpegView1.setAdjustHeight(true);
-        mjpegView1.setUrl(url);
+        mjpegView1.setUrl(location+"/stream");
         mjpegView1.startStream();
 
 
@@ -51,4 +104,6 @@ public class CameraGridActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
